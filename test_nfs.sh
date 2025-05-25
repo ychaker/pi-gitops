a719cf9951ba559a296c50eca929babff25aa392
Youssef Chaker <youssef@bearandgiraffe.com>
2025-05-25 12:15:14 -0400

    Adds script to test the nfs mounting from a Pi

---
+++ test_nfs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Script to test mounting the nfs share directory from a Raspberry Pi.
+#
+# Usage:
+#   chmod +x test_nfs.sh
+#   sudo ./test_nfs.sh
+#
+# OR:
+#   curl -O https://raw.githubusercontent.com/ychaker/pi-gitops/main/setup_pi.sh | bash
+#
+# Run this script directly on the Pi you wish to configure (preferably after first boot).
+
+set -e
+
+sudo mkdir -p /tmp/nfs-test
+sudo mount -t nfs 192.168.1.158:/volume1/k8s-share /tmp/nfs-test
+sudo touch /tmp/nfs-test/hello.txt
+ls -la /tmp/nfs-test
+sudo umount /tmp/nfs-test
